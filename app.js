53497b3705366743679fd8a6fa54718b3d76f6bb
Luis Henrique Jurasseck <luis.jurasseck@gmail.com>
2016-02-02 14:15:24 -0200

    tpl

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
@@ -9,40 +10,27 @@ var session = require('express-session');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.use(cookieParser());
+app.use(bodyParser());
+
 app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ secret: 'hueheubr' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./config/passport')(passport);
 
 var routes = require('./routes/index')(app, passport);
-var users = require('./routes/users');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({ secret: 'keyboard cat' }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', routes);
-// app.use('/users', users);
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -53,8 +41,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,32 @@
 module.exports = function(app, passport){
 
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'The ass revelation' });
-});
+	app.get('/', function(req, res, next) {
+		res.render('index', { title: 'The ass revelation' });
+	});
 
-app.get('/join', function(req,res){
-	res.render('join', {title: 'Join'});
-});
+	app.get('/join', function(req,res){
+		res.render('join', {title: 'Join'});
+	});
 
+	app.get('/join-spotify',
+		passport.authenticate('spotify', {scope: ['user-read-email', 'user-read-private'], showDialog: true}),
+		function(req, res){});
+
+	app.get('/callback',
+		passport.authenticate('spotify', { failureRedirect: '/login' }),
+		function(req, res) {
+			res.redirect('/home');
+	});
+
+	app.get('/home', isLoggedIn, function(req,res){
+		res.render('home', {title: 'Home', user: req.user._json});
+	});
 
 }
 
-// var express = require('express');
-// var router = express.Router();
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		return next();
 
-// /* GET home page. */
-
-
-// module.exports = router;
+	res.redirect('/');
+}
